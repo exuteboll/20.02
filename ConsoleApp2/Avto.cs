d51a2c4f6a8cdf86f7f73af5ac441312f04b01c8
exuteboll <exuteboll4@gmail.com>
2025-02-20 11:18:36 +0200

    Add files via upload

---
+++ ConsoleApp2/Avto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Avto
+    {
+        private string model;
+        private string color;
+        private double skor;
+        public double Skor
+        {
+            get { return skor; }
+            set
+            {
+                if (value < 60) skor = 60;
+                else if (value > 150) skor = 150;
+                else skor = value;
+            }
+        }
+
+
+        public string Model
+        {
+            get { return 
+                    (model!="")?model:"Неизвестно"; }
+            set { model = value; }
+        }
+        public string Color
+        {
+            get { return
+                    (color != "") ? color:"Неизвестно" ; }
+            set { color = value; }
+        }
+        public Avto() { }
+        public Avto(string model, string color, double skor)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Skor = skor;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Модель: {Model}\n Цвет: {Color}\n Скорость: {Skor} ");
+        }
+
+        
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto = new Avto("","Белый",80);
+            avto.ShowInfo();
+            Avto avto1 = new Avto("BMW", "", 200);
+            avto1.ShowInfo();
+            Avto avto2 = new Avto("Opel", "Красный", 90);
+            avto2.ShowInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
